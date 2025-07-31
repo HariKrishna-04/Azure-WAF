e9a48628286d51d0dd9368b4d5b393a579727f74
Mummidi Hari Krishna <mharikrishna0405@gmail.com>
2025-07-31 17:10:17 +0530

    Update backend.tf

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg1"      
-    storage_account_name = "tfstatestorageacct11"      
+    storage_account_name = "tfstatestorageacct1111"      
     container_name       = "tfstate11"                 
     key                  = "WAF.tfstate"  
   }
